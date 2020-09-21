30c8ffd336ffad67e72518c07be41dbed805704d
pkro <pkrombach@web.de>
2020-09-21 08:43:59 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const API = 'http://orangevalleycaa.org/api/videos';
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(API)
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((e) => console.log(e));
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Videos</h1>
       </header>
+      {data.length &&
+        data.map((entry) => (
+          <div key={entry.id}>
+            <h2>{entry.name}</h2>
+            <video
+              height={200}
+              controls
+              src={entry.video_url}
+              poster={entry.thumbnail}
+              preload={'metadata'}
+              alt={entry.description}
+            />
+          </div>
+        ))}
+      {!data.length && <>Loading...</>}
     </div>
   );
 }
